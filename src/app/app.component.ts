3d88fd43bf36648bf9f26521aabfea2fc11d4821
Am1vf <alvaro.am1vf@gmail.com>
2019-10-06 13:06:19 +0200

    adaptado a mock de issue ejemplo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit {
   title = 'prueba-irontec';
   apiUrl = '';
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['state', 'title', 'body', 'url'];
   dataSource = new MatTableDataSource;
 
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
@@ -26,29 +26,269 @@ export class AppComponent implements OnInit {
   }
 
   load() {
-    console.log('load: ' + this.apiUrl);
+    console.log('load: ' + this.apiUrl+'/issues'); //TODO
     
     this.dataSource.data = [
-      { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-      { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-      { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-      { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-      { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-      { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-      { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-      { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-      { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-      { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-      { position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na' },
-      { position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg' },
-      { position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al' },
-      { position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si' },
-      { position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P' },
-      { position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S' },
-      { position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl' },
-      { position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar' },
-      { position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K' },
-      { position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca' },
+      {
+        "id": 1,
+        "node_id": "MDU6SXNzdWUx",
+        "url": "https://api.github.com/repos/octocat/Hello-World/issues/1347",
+        "repository_url": "https://api.github.com/repos/octocat/Hello-World",
+        "labels_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/labels{/name}",
+        "comments_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/comments",
+        "events_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/events",
+        "html_url": "https://github.com/octocat/Hello-World/issues/1347",
+        "number": 1347,
+        "state": "open",
+        "title": "Found a bug",
+        "body": "I'm having a problem with this.",
+        "locked": true,
+        "active_lock_reason": "too heated",
+        "comments": 0,
+        "pull_request": {
+          "url": "https://api.github.com/repos/octocat/Hello-World/pulls/1347",
+          "html_url": "https://github.com/octocat/Hello-World/pull/1347",
+          "diff_url": "https://github.com/octocat/Hello-World/pull/1347.diff",
+          "patch_url": "https://github.com/octocat/Hello-World/pull/1347.patch"
+        },
+        "closed_at": null,
+        "created_at": "2011-04-22T13:33:48Z",
+        "updated_at": "2011-04-22T13:33:48Z"
+      },
+      {
+        "id": 2,
+        "node_id": "MDU6SXNzdWUx",
+        "url": "https://api.github.com/repos/octocat/Hello-World/issues/1347",
+        "repository_url": "https://api.github.com/repos/octocat/Hello-World",
+        "labels_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/labels{/name}",
+        "comments_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/comments",
+        "events_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/events",
+        "html_url": "https://github.com/octocat/Hello-World/issues/1347",
+        "number": 1348,
+        "state": "open",
+        "title": "Found another bug",
+        "body": "I'm having another problem with this.",
+        "locked": true,
+        "active_lock_reason": "too heated",
+        "comments": 0,
+        "pull_request": {
+          "url": "https://api.github.com/repos/octocat/Hello-World/pulls/1347",
+          "html_url": "https://github.com/octocat/Hello-World/pull/1347",
+          "diff_url": "https://github.com/octocat/Hello-World/pull/1347.diff",
+          "patch_url": "https://github.com/octocat/Hello-World/pull/1347.patch"
+        },
+        "closed_at": null,
+        "created_at": "2011-04-22T13:33:48Z",
+        "updated_at": "2011-04-22T13:33:48Z"
+      },
+      {
+        "id": 3,
+        "node_id": "MDU6SXNzdWUx",
+        "url": "https://api.github.com/repos/octocat/Hello-World/issues/1347",
+        "repository_url": "https://api.github.com/repos/octocat/Hello-World",
+        "labels_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/labels{/name}",
+        "comments_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/comments",
+        "events_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/events",
+        "html_url": "https://github.com/octocat/Hello-World/issues/1347",
+        "number": 1348,
+        "state": "open",
+        "title": "Found another one",
+        "body": "I'm having another problem with this.",
+        "locked": true,
+        "active_lock_reason": "too heated",
+        "comments": 0,
+        "pull_request": {
+          "url": "https://api.github.com/repos/octocat/Hello-World/pulls/1347",
+          "html_url": "https://github.com/octocat/Hello-World/pull/1347",
+          "diff_url": "https://github.com/octocat/Hello-World/pull/1347.diff",
+          "patch_url": "https://github.com/octocat/Hello-World/pull/1347.patch"
+        },
+        "closed_at": null,
+        "created_at": "2011-04-22T13:33:48Z",
+        "updated_at": "2011-04-22T13:33:48Z"
+      },
+      {
+        "id": 4,
+        "node_id": "MDU6SXNzdWUx",
+        "url": "https://api.github.com/repos/octocat/Hello-World/issues/1347",
+        "repository_url": "https://api.github.com/repos/octocat/Hello-World",
+        "labels_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/labels{/name}",
+        "comments_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/comments",
+        "events_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/events",
+        "html_url": "https://github.com/octocat/Hello-World/issues/1347",
+        "number": 1348,
+        "state": "open",
+        "title": "And another one",
+        "body": "I'm having another problem with this.",
+        "locked": true,
+        "active_lock_reason": "too heated",
+        "comments": 0,
+        "pull_request": {
+          "url": "https://api.github.com/repos/octocat/Hello-World/pulls/1347",
+          "html_url": "https://github.com/octocat/Hello-World/pull/1347",
+          "diff_url": "https://github.com/octocat/Hello-World/pull/1347.diff",
+          "patch_url": "https://github.com/octocat/Hello-World/pull/1347.patch"
+        },
+        "closed_at": null,
+        "created_at": "2011-04-22T13:33:48Z",
+        "updated_at": "2011-04-22T13:33:48Z"
+      },
+      {
+        "id": 5,
+        "node_id": "MDU6SXNzdWUx",
+        "url": "https://api.github.com/repos/octocat/Hello-World/issues/1347",
+        "repository_url": "https://api.github.com/repos/octocat/Hello-World",
+        "labels_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/labels{/name}",
+        "comments_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/comments",
+        "events_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/events",
+        "html_url": "https://github.com/octocat/Hello-World/issues/1347",
+        "number": 1348,
+        "state": "open",
+        "title": "And another one",
+        "body": "I'm having another problem with this.",
+        "locked": true,
+        "active_lock_reason": "too heated",
+        "comments": 0,
+        "pull_request": {
+          "url": "https://api.github.com/repos/octocat/Hello-World/pulls/1347",
+          "html_url": "https://github.com/octocat/Hello-World/pull/1347",
+          "diff_url": "https://github.com/octocat/Hello-World/pull/1347.diff",
+          "patch_url": "https://github.com/octocat/Hello-World/pull/1347.patch"
+        },
+        "closed_at": null,
+        "created_at": "2011-04-22T13:33:48Z",
+        "updated_at": "2011-04-22T13:33:48Z"
+      },
+      {
+        "id": 6,
+        "node_id": "MDU6SXNzdWUx",
+        "url": "https://api.github.com/repos/octocat/Hello-World/issues/1347",
+        "repository_url": "https://api.github.com/repos/octocat/Hello-World",
+        "labels_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/labels{/name}",
+        "comments_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/comments",
+        "events_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/events",
+        "html_url": "https://github.com/octocat/Hello-World/issues/1347",
+        "number": 1348,
+        "state": "open",
+        "title": "And another one",
+        "body": "I'm having another problem with this.",
+        "locked": true,
+        "active_lock_reason": "too heated",
+        "comments": 0,
+        "pull_request": {
+          "url": "https://api.github.com/repos/octocat/Hello-World/pulls/1347",
+          "html_url": "https://github.com/octocat/Hello-World/pull/1347",
+          "diff_url": "https://github.com/octocat/Hello-World/pull/1347.diff",
+          "patch_url": "https://github.com/octocat/Hello-World/pull/1347.patch"
+        },
+        "closed_at": null,
+        "created_at": "2011-04-22T13:33:48Z",
+        "updated_at": "2011-04-22T13:33:48Z"
+      },
+      {
+        "id": 7,
+        "node_id": "MDU6SXNzdWUx",
+        "url": "https://api.github.com/repos/octocat/Hello-World/issues/1347",
+        "repository_url": "https://api.github.com/repos/octocat/Hello-World",
+        "labels_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/labels{/name}",
+        "comments_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/comments",
+        "events_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/events",
+        "html_url": "https://github.com/octocat/Hello-World/issues/1347",
+        "number": 1348,
+        "state": "open",
+        "title": "And another one",
+        "body": "I'm having another problem with this.",
+        "locked": true,
+        "active_lock_reason": "too heated",
+        "comments": 0,
+        "pull_request": {
+          "url": "https://api.github.com/repos/octocat/Hello-World/pulls/1347",
+          "html_url": "https://github.com/octocat/Hello-World/pull/1347",
+          "diff_url": "https://github.com/octocat/Hello-World/pull/1347.diff",
+          "patch_url": "https://github.com/octocat/Hello-World/pull/1347.patch"
+        },
+        "closed_at": null,
+        "created_at": "2011-04-22T13:33:48Z",
+        "updated_at": "2011-04-22T13:33:48Z"
+      },
+      {
+        "id": 8,
+        "node_id": "MDU6SXNzdWUx",
+        "url": "https://api.github.com/repos/octocat/Hello-World/issues/1347",
+        "repository_url": "https://api.github.com/repos/octocat/Hello-World",
+        "labels_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/labels{/name}",
+        "comments_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/comments",
+        "events_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/events",
+        "html_url": "https://github.com/octocat/Hello-World/issues/1347",
+        "number": 1348,
+        "state": "open",
+        "title": "And another one",
+        "body": "I'm having another problem with this.",
+        "locked": true,
+        "active_lock_reason": "too heated",
+        "comments": 0,
+        "pull_request": {
+          "url": "https://api.github.com/repos/octocat/Hello-World/pulls/1347",
+          "html_url": "https://github.com/octocat/Hello-World/pull/1347",
+          "diff_url": "https://github.com/octocat/Hello-World/pull/1347.diff",
+          "patch_url": "https://github.com/octocat/Hello-World/pull/1347.patch"
+        },
+        "closed_at": null,
+        "created_at": "2011-04-22T13:33:48Z",
+        "updated_at": "2011-04-22T13:33:48Z"
+      },
+      {
+        "id": 9,
+        "node_id": "MDU6SXNzdWUx",
+        "url": "https://api.github.com/repos/octocat/Hello-World/issues/1347",
+        "repository_url": "https://api.github.com/repos/octocat/Hello-World",
+        "labels_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/labels{/name}",
+        "comments_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/comments",
+        "events_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/events",
+        "html_url": "https://github.com/octocat/Hello-World/issues/1347",
+        "number": 1348,
+        "state": "open",
+        "title": "And another one",
+        "body": "I'm having another problem with this.",
+        "locked": true,
+        "active_lock_reason": "too heated",
+        "comments": 0,
+        "pull_request": {
+          "url": "https://api.github.com/repos/octocat/Hello-World/pulls/1347",
+          "html_url": "https://github.com/octocat/Hello-World/pull/1347",
+          "diff_url": "https://github.com/octocat/Hello-World/pull/1347.diff",
+          "patch_url": "https://github.com/octocat/Hello-World/pull/1347.patch"
+        },
+        "closed_at": null,
+        "created_at": "2011-04-22T13:33:48Z",
+        "updated_at": "2011-04-22T13:33:48Z"
+      },
+      {
+        "id": 10,
+        "node_id": "MDU6SXNzdWUx",
+        "url": "https://api.github.com/repos/octocat/Hello-World/issues/1347",
+        "repository_url": "https://api.github.com/repos/octocat/Hello-World",
+        "labels_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/labels{/name}",
+        "comments_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/comments",
+        "events_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/events",
+        "html_url": "https://github.com/octocat/Hello-World/issues/1347",
+        "number": 1348,
+        "state": "open",
+        "title": "And another one",
+        "body": "I'm having another problem with this.",
+        "locked": true,
+        "active_lock_reason": "too heated",
+        "comments": 0,
+        "pull_request": {
+          "url": "https://api.github.com/repos/octocat/Hello-World/pulls/1347",
+          "html_url": "https://github.com/octocat/Hello-World/pull/1347",
+          "diff_url": "https://github.com/octocat/Hello-World/pull/1347.diff",
+          "patch_url": "https://github.com/octocat/Hello-World/pull/1347.patch"
+        },
+        "closed_at": null,
+        "created_at": "2011-04-22T13:33:48Z",
+        "updated_at": "2011-04-22T13:33:48Z"
+      }
     ];
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 // material
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
@@ -20,10 +21,12 @@ import { MatButtonModule } from '@angular/material/button';
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     MatCardModule,
     MatTableModule,
     MatPaginatorModule,
     MatInputModule,
+    MatFormFieldModule,
     MatButtonModule
   ],
   providers: [],
