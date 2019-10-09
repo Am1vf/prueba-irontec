9948d4bb1a5d3b6de326e9c83e24291de6a3053a
Am1vf <alvaro.am1vf@gmail.com>
2019-10-09 21:22:14 +0200

    todos los arreglos de vista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ export class AppComponent implements OnInit {
   apiUrl = '';
   issues$: Observable<any>;
 
-  displayedColumns: string[] = ['state', 'title', 'body', 'url'];
+  displayedColumns: string[] = ['state', 'title', 'url'];
   dataSource = new MatTableDataSource<Observable<any>>();
 
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
